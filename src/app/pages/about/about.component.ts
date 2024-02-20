46a91d797bd44fef75135715226daf1569ee95b6
nathm@NATHBOOK <nathaliametring@gmail.com>
2024-02-20 16:47:41 -0300

    Mudança do banner na página About

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { PrincipalCardComponent } from '../../components/principal-card/principal-card.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [PrincipalCardComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
 })
-export class AboutComponent {}
+export class AboutComponent { }
